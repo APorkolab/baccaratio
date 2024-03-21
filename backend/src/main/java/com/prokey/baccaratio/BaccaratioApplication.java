6686aa0dc9eb2bb2a50a4e1952d6de1f9009386a
APorkolab <adam.porkolab@gmail.com>
2024-03-21 19:31:08 +0100

    First commit.

---
+++ backend/src/main/java/com/prokey/baccaratio/BaccaratioApplication.java
@@ -0,0 +1,13 @@
+package com.prokey.baccaratio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaccaratioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaccaratioApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/prokey/baccaratio/BaccaratioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prokey.baccaratio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaccaratioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
