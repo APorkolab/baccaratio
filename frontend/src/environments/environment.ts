469dccb35f0c5829d835e7b115691b19a259e371
APorkolab <adam.porkolab@gmail.com>
2024-04-03 18:57:41 +0200

    Modified the Environment.ts because of the deploy.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-	production: false,
-	apiUrl: 'https://baccaratio.appspot.com/baccarat',
+	production: true,
+	apiUrl: 'https://baccaratio-wandering-sun-796.fly.dev/baccarat',
 };
