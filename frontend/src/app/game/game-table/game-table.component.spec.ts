d768cc5f4f8f7e7c8b4e999273468abc36c5300e
Adam Porkolab <73812292+APorkolab@users.noreply.github.com>
2025-09-06 18:49:45 +0200

    Update game-table.component.spec.ts

--- frontend/src/app/game/game-table/game-table.component.spec.ts
+++ frontend/src/app/game/game-table/game-table.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GameTableComponent } from './game-table.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('GameTableComponent', () => {
   let component: GameTableComponent;
