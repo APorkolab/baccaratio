26dcc852b7ec8528223aa7bbf5113086c76859f6
Adam Porkolab <73812292+APorkolab@users.noreply.github.com>
2025-09-06 18:41:14 +0200

    Update player-status.component.spec.ts

--- frontend/src/app/game/player-status/player-status.component.spec.ts
+++ frontend/src/app/game/player-status/player-status.component.spec.ts
@@ -1,15 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PlayerStatusComponent } from './player-status.component';
 
 describe('PlayerStatusComponent', () => {
   let component: PlayerStatusComponent;
   let fixture: ComponentFixture<PlayerStatusComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [PlayerStatusComponent]
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PlayerStatusComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(PlayerStatusComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
